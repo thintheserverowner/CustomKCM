43bcbf7b013bda01158716791e810e9c88af70d7
Thinc <Thinc@HP-PAVILION-LAP>
2022-03-12 12:19:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+using DiskCardGame;
+using HarmonyLib;
+using UnityEngine;
+using APIPlugin;
+using KayceesModModifierAPI;
+
+namespace CustomKaycee
+{ KayceeModModifier.AddStarterDeckOption("Rare_Deck" , "Rare.png", new List<string> { "Urayuli", "Goat", "Goat" }, "8",);}
